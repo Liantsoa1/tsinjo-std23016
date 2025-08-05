c255f412d41faaeca13abb19d1f0ef1734a9a01b
liantsoa <hei.liantsoa@gmail.com>
2025-08-05 15:35:18 +0300

    chore: added jpa to all model

--- src/main/java/com/my/company/model/Beneficiary.java
+++ src/main/java/com/my/company/model/Beneficiary.java
@@ -4,9 +4,22 @@ package com.my.company.model;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "beneficiary")
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Beneficiary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "beneficiary_id")
     private Long beneficiaryId;
-    private String email;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
 }
--- src/main/java/com/my/company/model/Payment.java
+++ src/main/java/com/my/company/model/Payment.java
@@ -1,15 +1,41 @@
 package com.my.company.model;
 
+import jakarta.persistence.EnumType;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@AllArgsConstructor
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "payment")
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Payment {
-    private String payementId;
+
+    @Id
+    @Column(name = "payment_id")
+    private String paymentId;
+
+    @Column(name = "payer_email", nullable = false)
+    private String payerEmail;
+
+    @Column(name = "payment_date", nullable = false)
     private LocalDate paymentDate;
+
+    @Column(name = "payment_amount", nullable = false)
     private int paymentAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "means_of_payment", nullable = false)
     private MeansOfPayment meansOfPayment;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "verification_status", nullable = false)
+    private VerificationStatus verificationStatus;
 }
+
